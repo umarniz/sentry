d920c8f8251ecf6afc01cd20a814b2e494429c00
Danny Lee <dlee@sentry.io>
2020-08-03 20:59:49 -0700

    feat(dashboard): Basic structure for cards in dashboard

--- src/sentry/static/sentry/app/views/teamsDashboard/cards/index.tsx
+++ src/sentry/static/sentry/app/views/teamsDashboard/cards/index.tsx
@@ -2,48 +2,26 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import AppComponentCard from 'app/components/card';
-import space from 'app/styles/space';
 // import theme from 'app/utils/theme';
 
-import CardAddNew from './cardAddNew';
-
 type Prop = {
-  column: 1 | 2 | 3; // Size of the card
+  columnSpan: 1 | 2 | 3; // Size of the card
   content?: React.ReactNode;
 };
 
 class Card extends React.Component<Prop> {
   render() {
-    return <CardWrapper {...this.props}>{this.props.children || 'asd1'}</CardWrapper>;
+    const {columnSpan} = this.props;
+
+    return (
+      <CardWrapper columnSpan={columnSpan}>{this.props.children || 'asd1'}</CardWrapper>
+    );
   }
 }
 
 export default Card;
-export const Grid = props => {
-  return (
-    <Container>
-      <Card column={1} />
-      <Card column={1} />
-      <Card column={1} />
-      <Card column={2} />
-      <Card column={1} />
-      <Card column={3} />
-      <Card column={1} />
-      <Card column={2} />
-      {props.children}
-      <CardAddNew />
-    </Container>
-  );
-};
 
-const Container = styled('div')`
-  display: grid;
-  grid-template-columns: repeat(3, minmax(100px, 1fr));
-  grid-gap: ${space(3)};
-`;
-
-const CardWrapper = styled(AppComponentCard)`
-  /* display: block;
-  border: ${p => p.theme.borderRadius};
-  border-radius: ${p => p.theme.borderRadius}; */
+const CardWrapper = styled(AppComponentCard)<{columnSpan: number}>`
+  min-height: 200px; /* Allow taller rows based on content */
+  grid-column: auto / span ${p => p.columnSpan};
 `;
